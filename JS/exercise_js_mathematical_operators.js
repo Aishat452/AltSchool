27df0ce6ba45dd68eaa9c48de49c698e9e718170
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2024-01-04 11:43:35 +0100

    first commit

---
+++ JS/exercise_js_mathematical_operators.js
@@ -0,0 +1,73 @@
+/*
+    Basic operators: 
+    +, -, *, /, % 
+
+    Assignment operators: 
+    =, +=, -=, *=, /=, %=
+
+    Comparison operators: 
+    ==, ===, !=, !==, >, <, >=, <=, &&, ||, !, ?
+
+    Logical operators: 
+    &&, ||, !, ?
+
+    Conditional operators: 
+    ?, :,
+
+    Unary operators: 
+    ++, --, +, -
+
+    Bitwise operators: 
+    &, |, ^, <<, >>, >>>, ~
+
+    Ternary operators: 
+    ?
+    
+    shorthand operators:
+    +=, -=, *=, /=, %=
+
+    increment/decrement operators:  
+    ++, --
+*/
+
+// We can use the + operator to add two numbers or concactenate two strings
+
+// let x = 10;
+// let y = 20;
+
+// console.log(x + y); // 30 ` ${}`
+// console.log(y % x);
+
+// const my_Name = 'Remi Oje';
+// const age = 20;
+
+// console.log(`My name is ${my_Name} and I am ${age} years old`); // My name is Remi Oje and I am 20 years old
+
+
+
+// let user_input = prompt('Enter a number: ');
+
+// if (user_input % 2 == 0) {
+//     alert('Number is even');
+// } else {
+//     alert('Number is odd');
+// }
+
+
+let a = 20;
+
+let b = 10;
+
+// let c = a + b;
+// let c = a - b;
+// let c = a * b;
+// let c = a / b;
+// let c = a % b;
+
+
+
+console.log(a += b); // a + b = c 
+console.log(a -= b); // a - b = c
+console.log(a *= b); // a * b = c
+console.log(a /= b); // a / b = c
+console.log(a %= b); // a % b = c
